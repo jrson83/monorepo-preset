a386a29b0fa1ee2473d8eca3e73f4f64294aa61e
jrson83 <me@jrson.de>
2022-12-26 11:31:12 +0100

    test

--- packages/presets/preset.ts
+++ packages/presets/preset.ts
@@ -1,13 +1,17 @@
 interface Options {
-  ui: 'preact' | 'react' | 'vue'
+  ui: 'preact' | 'react' | 'vue' | undefined
 }
 
 export default definePreset<Options>({
 	name: 'presets',
 	options: {
-		ui: 'react'
+		ui: undefined
 	},
 	handler: async({ options }) => {
+		
+		if (!options.ui) {
+			return console.log("no ui specified")
+		}
 		
 		await extractTemplates({
 			title: 'extract templates',
